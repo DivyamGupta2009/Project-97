d8eba1cd77fd973f5e3bcff005bdec6b6d12d746
DivyamGupta2009 <62936715+DivyamGupta2009@users.noreply.github.com>
2021-07-19 20:54:01 -0700

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,24 @@
+import random
+n = random.randint(1,10)
+guess = 5
+while(guess>0):
+    a = int(input("Guess a number(between 1 and 10): "))
+    if(a>10):
+        print("Guess a number between 1 and 10!")
+        guess = guess-1
+    elif(a<0):
+        print("Guess a number between 1 and 10!")
+        guess = guess-1
+    elif(a<n):
+        print("Guess a higher number.")
+        guess = guess-1
+    elif(a>n):
+        print("Guess a lower number.")
+        guess = guess-1
+    elif(guess<=0):
+        print("Oh no. You ran out of guesses!")
+    else:
+        
+        print("Congrats! You won!")
+        
+    
